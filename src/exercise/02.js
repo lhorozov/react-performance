23a1d2bd18b83c68de269fcb40fccd439cc05936
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-01-22 20:17:41 +0200

    useMemo for expensive calculations-extra credit-2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,8 +3,9 @@
 
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
-import {getItems} from '../filter-cities'
-import {useForceRerender} from '../utils'
+import {getItems} from '../workerized-filter-cities'
+// import {getItems} from '../filter-cities'
+import {useAsync, useForceRerender} from '../utils'
 
 function Menu({
   items,
@@ -60,10 +61,10 @@ function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
 
-  const namedFF = () => getItems(inputValue)
-
-  const allItems = React.useMemo(namedFF, [inputValue])
-  // const allItems = getItems(inputValue)
+  const {data: allItems, run} = useAsync({data: [], status: 'pending'})
+  React.useEffect(() => {
+    run(getItems(inputValue))
+  }, [inputValue, run])
   const items = allItems.slice(0, 100)
 
   const {
