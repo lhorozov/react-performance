25d790555e095291e8139a7a8dd449970b968f82
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-01-22 10:50:18 +0200

    Code splitting

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,46 +2,38 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 💣 remove this import
-import Globe from '../globe'
-
-// 🐨 use React.lazy to create a Globe component which uses a dynamic import
-// to get the Globe component from the '../globe' module.
+// import Globe from '../globe'
+const Globe = React.lazy(() => import('../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
 
-  // 🐨 wrap the code below in a <React.Suspense /> component
-  // with a fallback.
-  // 💰 try putting it in a few different places and observe how that
-  // impacts the user experience.
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        height: '100%',
-        padding: '2rem',
-      }}
-    >
-      <label style={{marginBottom: '1rem'}}>
-        <input
-          type="checkbox"
-          checked={showGlobe}
-          onChange={e => setShowGlobe(e.target.checked)}
-        />
-        {' show globe'}
-      </label>
-      <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          height: '100%',
+          padding: '2rem',
+        }}
+      >
+        <label style={{marginBottom: '1rem'}}>
+          <input
+            type="checkbox"
+            checked={showGlobe}
+            onChange={e => setShowGlobe(e.target.checked)}
+          />
+          {' show globe'}
+        </label>
+        <div style={{width: 400, height: 400}}>
+          {showGlobe ? <Globe /> : null}
+        </div>
       </div>
-    </div>
+    </React.Suspense>
   )
 }
-// 🦉 Note that if you're not on the isolated page, then you'll notice that this
-// app actually already has a React.Suspense component higher up in the tree
-// where this component is rendered, so you *could* just rely on that one.
 
 export default App
